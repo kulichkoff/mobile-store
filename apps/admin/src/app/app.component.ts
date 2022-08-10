fb67cf42560f72c7ff80ccd0b296185f7348164a
kulichkoff <kulichkov@background.digital>
2022-08-10 17:35:07 +0300

    services lib

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import {
+    ChangeDetectorRef,
+    Component,
+    OnInit,
+} from '@angular/core';
+import {
+    FormBuilder,
+    FormGroup,
+} from '@angular/forms';
+import {
+    HttpClient,
+    HttpParams,
+} from '@angular/common/http';
+import { take } from 'rxjs';
+import { QuerySearchParamsService } from './core/services/query-search-params.service';
+import { EmptyService } from 'libs/shared-services/src';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'admin';
+export class AppComponent implements OnInit {
+
+    public searchForm: FormGroup = this.fb.group({
+        searchText: '',
+        currency: '',
+    });
+
+    constructor(
+        private fb: FormBuilder,
+        private changeDetectorRef: ChangeDetectorRef,
+        private http: HttpClient,
+        private querySearchParamsService: QuerySearchParamsService,
+        private empty: EmptyService,
+    ) {}
+
+    public ngOnInit() {
+        this.searchForm.valueChanges
+            .subscribe((value) => { this.onFormChanged(value) });
+
+        console.log(this.empty.getHello());
+    }
+
+    public onBtnClicked() {
+        this.changeDetectorRef.detectChanges();
+
+        const params = new HttpParams({
+            fromObject: {
+                q: this.searchForm.value.searchText,
+                currency: this.searchForm.value.currency,
+            },
+        });
+
+        this.http.get('http://kulichkoff.space:3030/mobiles', { params })
+            .pipe(
+                take(1),
+            )
+            .subscribe((data) => {
+                console.log(data);
+            });
+    }
+
+    public onFormChanged(formValue: FormGroup['value']) {
+        this.querySearchParamsService.setQuery(
+            {
+                q: formValue.searchText,
+                currency: formValue.currency,
+            },
+        );
+    }
+
+    public onRubBtnClicked() {
+        this.searchForm.patchValue({ currency: 'RUB' });
+    }
 }
+
--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -3,12 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { CoreModule } from './core/core.module';
+import {
+    FormsModule,
+    ReactiveFormsModule,
+} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { SharedServicesModule } from '@app/shared-services';
 
 @NgModule({
     declarations: [AppComponent],
     imports: [
         BrowserModule,
         RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+        ButtonModule,
+        RippleModule,
+        CoreModule,
+        ReactiveFormsModule,
+        FormsModule,
+        HttpClientModule,
+        SharedServicesModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
